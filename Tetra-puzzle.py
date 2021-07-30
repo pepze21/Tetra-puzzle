46291cc7e56b8b609b45e0e0ee4a9e8d22c39142
pepze21 <pepze21@gmail.com>
2021-07-30 23:45:21 +0900

    add Do_X functions

--- Tetra-puzzle.py
+++ Tetra-puzzle.py
@@ -1,4 +1,5 @@
-### Tetra Puzzle ver.210727.02 rough draft, add T1~4 cases
+### Tetra Puzzle ver.210727.04 rough draft, shortened the length of the code by using indices
+
 
 import copy # to use deepcopy()
 
@@ -10,34 +11,18 @@ frame = []
 ## Class & Functions
 class Node() :
     def __init__(self):
-        self.frame = None # remaining space in the frame
-        self.data = None # puzzles filled in initial frame
-        self.S = None
-        self.Z1 = None
-        self.Z2 = None
-        self.J1 = None
-        self.J2 = None
-        self.J3 = None
-        self.L1 = None
-        self.L2 = None
-        self.L3 = None
-        self.T1 = None
-        self.T2 = None
-        self.T3 = None
-        self.T4 = None
-        self.I1 = None
-        self.I2 = None
-        self.O = None
+        self.frame = None # frame state
+        self.blockData = [] # blocks filled in initial frame
+        self.case = [None for _ in range(16)] #1S2Z3J3L4T2I1O:16cases
+        self.cnt = [0 for _ in range(16)]
 
-        self.cnt_S = None # num of S shape blocks
-        self.cnt_Z = None
-        self.cnt_J = None
-        self.cnt_L = None
-        self.cnt_T = None
-        self.cnt_I = None
-        self.cnt_O = None
+def findNext(frame):
+    for i in range(3, SIZE + 3):
+        for j in range(3, SIZE + 3):
+            if (frame[i][j] == 0):
+                return i, j # return (i, j) of tuple type
 
-def inEmpty(i, j) :
+def isEmpty(i, j) :
     return (frame[i][j] == 0)
 
 def can_S(i, j, frame) :
@@ -103,19 +88,157 @@ def do_S(i,j,frame):
     current.frame[i+1][j+1] = 1
     current.frame[i+2][i+1] = 1
 
-'''
+
 def do_Z1 :
+    global current
+    current.Z1 = copy.deepcopy(current)
+    current.data.append([i,j,'Z1'])
+    current.cnt_Z1 += 1
+    current.frame = copy.deepcopy(frame)
+    current.frame[i][j] = 1
+    current.frame[i+1][j] = 1
+    current.frame[i+1][j+1] = 1
+    current.frame[i+2][i+1] = 1
 def do_Z2 :
+    global current
+    current.Z2 = copy.deepcopy(current)
+    current.data.append([i,j,'Z2'])
+    current.cnt_Z2 += 1
+    current.frame = copy.deepcopy(frame)
+    current.frame[i][j] = 1
+    current.frame[i+1][j] = 1
+    current.frame[i+1][j+1] = 1
+    current.frame[i+2][i+1] = 1
 def do_J1 :
+    global current
+    current.J1 = copy.deepcopy(current)
+    current.data.append([i,j,'J1'])
+    current.cnt_J1 += 1
+    current.frame = copy.deepcopy(frame)
+    current.frame[i][j] = 1
+    current.frame[i+1][j] = 1
+    current.frame[i+1][j+1] = 1
+    current.frame[i+2][i+1] = 1
 def do_J2 :
+    global current
+    current.J2 = copy.deepcopy(current)
+    current.data.append([i,j,'J2'])
+    current.cnt_J2 += 1
+    current.frame = copy.deepcopy(frame)
+    current.frame[i][j] = 1
+    current.frame[i+1][j] = 1
+    current.frame[i+1][j+1] = 1
+    current.frame[i+2][i+1] = 1
 def do_J3 :
+    global current
+    current.J3 = copy.deepcopy(current)
+    current.data.append([i,j,'J3'])
+    current.cnt_J3 += 1
+    current.frame = copy.deepcopy(frame)
+    current.frame[i][j] = 1
+    current.frame[i+1][j] = 1
+    current.frame[i+1][j+1] = 1
+    current.frame[i+2][i+1] = 1
 def do_L1 :
+    global current
+    current.L1 = copy.deepcopy(current)
+    current.data.append([i,j,'L1'])
+    current.cnt_S += 1
+    current.frame = copy.deepcopy(frame)
+    current.frame[i][j] = 1
+    current.frame[i+1][j] = 1
+    current.frame[i+1][j+1] = 1
+    current.frame[i+2][i+1] = 1
 def do_L2 :
+    global current
+    current.L2 = copy.deepcopy(current)
+    current.data.append([i,j,'L2'])
+    current.cnt_L2 += 1
+    current.frame = copy.deepcopy(frame)
+    current.frame[i][j] = 1
+    current.frame[i+1][j] = 1
+    current.frame[i+1][j+1] = 1
+    current.frame[i+2][i+1] = 1
 def do_L3 :
+    global current
+    current.L3 = copy.deepcopy(current)
+    current.data.append([i,j,'L3'])
+    current.cnt_L3 += 1
+    current.frame = copy.deepcopy(frame)
+    current.frame[i][j] = 1
+    current.frame[i+1][j] = 1
+    current.frame[i+1][j+1] = 1
+    current.frame[i+2][i+1] = 1
+def do_T1 :
+    global current
+    current.T1 = copy.deepcopy(current)
+    current.data.append([i,j,'T1'])
+    current.cnt_T1 += 1
+    current.frame = copy.deepcopy(frame)
+    current.frame[i][j] = 1
+    current.frame[i+1][j] = 1
+    current.frame[i+1][j+1] = 1
+    current.frame[i+2][i+1] = 1
+def do_T2 :
+    global current
+    current.T2 = copy.deepcopy(current)
+    current.data.append([i,j,'T2'])
+    current.cnt_T2 += 1
+    current.frame = copy.deepcopy(frame)
+    current.frame[i][j] = 1
+    current.frame[i+1][j] = 1
+    current.frame[i+1][j+1] = 1
+    current.frame[i+2][i+1] = 1
+def do_T3 :
+    global current
+    current.T3 = copy.deepcopy(current)
+    current.data.append([i,j,'T3'])
+    current.cnt_T3 += 1
+    current.frame = copy.deepcopy(frame)
+    current.frame[i][j] = 1
+    current.frame[i+1][j] = 1
+    current.frame[i+1][j+1] = 1
+    current.frame[i+2][i+1] = 1
+def do_T4 :
+    global current
+    current.T4 = copy.deepcopy(current)
+    current.data.append([i,j,'T4'])
+    current.cnt_T4 += 1
+    current.frame = copy.deepcopy(frame)
+    current.frame[i][j] = 1
+    current.frame[i+1][j] = 1
+    current.frame[i+1][j+1] = 1
+    current.frame[i+2][i+1] = 1    
 def do_I1 :
+    global current
+    current.I1 = copy.deepcopy(current)
+    current.data.append([i,j,'I1'])
+    current.cnt_I1 += 1
+    current.frame = copy.deepcopy(frame)
+    current.frame[i][j] = 1
+    current.frame[i+1][j] = 1
+    current.frame[i+1][j+1] = 1
+    current.frame[i+2][i+1] = 1
 def do_I2 :
+    global current
+    current.I2 = copy.deepcopy(current)
+    current.data.append([i,j,'I2'])
+    current.cnt_I2 += 1
+    current.frame = copy.deepcopy(frame)
+    current.frame[i][j] = 1
+    current.frame[i+1][j] = 1
+    current.frame[i+1][j+1] = 1
+    current.frame[i+2][i+1] = 1
 def do_O :
-'''
+    global current
+    current.O = copy.deepcopy(current)
+    current.data.append([i,j,'O'])
+    current.cnt_O += 1
+    current.frame = copy.deepcopy(frame)
+    current.frame[i][j] = 1
+    current.frame[i+1][j] = 1
+    current.frame[i+1][j+1] = 1
+    current.frame[i+2][i+1] = 1
 
 
 
