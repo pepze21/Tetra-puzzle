c96d7e250299b2eb20b03cdcb57034d74e97bee6
pepze21 <pepze21@gmail.com>
2021-07-31 06:17:08 +0900

    big change of algorithm. no Node() class

--- Tetra-puzzle.py
+++ Tetra-puzzle.py
@@ -6,15 +6,19 @@ import copy # to use deepcopy()
 ## Variables
 SIZE = 9
 frame = []
-
+history = []
+cnt = [0 for _ in range(16)]
 
 ## Class & Functions
+
+'''
 class Node() :
     def __init__(self):
         self.frame = None # frame state
         self.blockData = [] # blocks filled in initial frame
-        self.case = [None for _ in range(16)] #1S2Z3J3L4T2I1O:16cases
+        self.type = [None for _ in range(16)] #1S2Z3J3L4T2I1O:16cases
         self.cnt = [0 for _ in range(16)]
+'''
 
 def findNext(frame):
     for i in range(3, SIZE + 3):
@@ -25,221 +29,150 @@ def findNext(frame):
 def isEmpty(i, j) :
     return (frame[i][j] == 0)
 
-def can_S(i, j, frame) :
-    return (frame[i][j] + frame[i+1][j] + frame[i+1][j+1] + frame[i+2][j+1] == 0)
 
-def can_Z1(i, j) :
-    return (frame[i][j] + frame[i][j+1] + frame[i+1][j+1] + frame[i+1][j+2] == 0)
-    
-def can_Z2(i, j) :
-    return (frame[i][j] + frame[i+1][j] + frame[i+1][j-1] + frame[i+2][j-1] == 0)
-    
-def can_J1(i, j) :
-    return (frame[i][j] + frame[i][j+1] + frame[i][j+2] + frame[i+1][j+2] == 0)
-
-def can_J2(i, j) :
-    return (frame[i][j] + frame[i+1][j] + frame[i+2][j] + frame[i+2][j-1] == 0)
-    
-def can_J3(i, j) :
-    return (frame[i][j] + frame[i+1][j] + frame[i+1][j+1] + frame[i+1][j+2] == 0)
-    
-def can_L1(i, j) :
-    return (frame[i][j] + frame[i+1][j] + frame[i+2][j] + frame[i+2][j+1] == 0)
-   
-def can_L2(i, j) :
-    return (frame[i][j] + frame[i][j+1] + frame[i+1][j+1] + frame[i+2][j+1] == 0)
-
-def can_L3(i, j) :
-    return (frame[i][j] + frame[i+1][j] + frame[i+1][j-1] + frame[i+1][j-2] == 0)
-
-def can_T1(i, j) :
-    return (frame[i][j] + frame[i][j+1] + frame[i][j+2] + frame[i+1][j+1] == 0)
-
-def can_T2(i, j) :
-    return (frame[i][j] + frame[i+1][j] + frame[i+2][j] + frame[i+1][j+1] == 0)
-
-def can_T3(i, j) :
-    return (frame[i][j] + frame[i+1][j] + frame[i+2][j] + frame[i+1][j-1] == 0)
-
-def can_T4(i, j) :
-    return (frame[i][j] + frame[i+1][j] + frame[i+1][j+1] + frame[i+1][j-1] == 0)
-
-def can_I1(i, j) :
-    return (frame[i][j] + frame[i][j+1] + frame[i][j+2] + frame[i][j+3] == 0)
-
-def can_I2(i, j) :
-    return (frame[i][j] + frame[i+1][j] + frame[i+2][j] + frame[i+3][j] == 0)
-    
-def can_O(i, j) :
-    return (frame[i][j] + frame[i][j+1] + frame[i+1][j+1] + frame[i+1][j] == 0)
-
-
-root = Node()
-current = root
-
-def do_S(i,j,frame):
-    global current
-    current.S = copy.deepcopy(current)
-    current.data.append([i,j,'S'])
-    current.cnt_S += 1
-    current.frame = copy.deepcopy(frame)
-    current.frame[i][j] = 1
-    current.frame[i+1][j] = 1
-    current.frame[i+1][j+1] = 1
-    current.frame[i+2][i+1] = 1
+# typeNo : 0:S  / 1:Z1 /  2:Z2 /  3:J1 /  4:J2 /  5:J3 /  6:L1 /  7:L2
+#          8:L3 / 9:T1 / 10:T2 / 11:T3 / 12:T4 / 13:I1 / 14:I2 / 15:O
+def canAdd(typeNo, i, j, frame) :
+    if typeNo == 0 :
+        return (frame[i][j] + frame[i+1][j] + \
+                frame[i+1][j+1] + frame[i+2][j+1] == 0)                
+    elif typeNo == 1 :
+        return (frame[i][j] + frame[i][j+1] + \
+                frame[i+1][j+1] + frame[i+1][j+2] == 0)
+    elif typeNo == 2 :
+        return (frame[i][j] + frame[i+1][j] + \
+                frame[i+1][j-1] + frame[i+2][j-1] == 0)    
+    elif typeNo == 3 :
+        return (frame[i][j] + frame[i][j+1] + \
+                frame[i][j+2] + frame[i+1][j+2] == 0)
+    elif typeNo == 4 :
+        return (frame[i][j] + frame[i+1][j] + \
+                frame[i+2][j] + frame[i+2][j-1] == 0)    
+    elif typeNo == 5 :
+        return (frame[i][j] + frame[i+1][j] + \
+                frame[i+1][j+1] + frame[i+1][j+2] == 0)    
+    elif typeNo == 6 :
+        return (frame[i][j] + frame[i+1][j] + \
+                frame[i+2][j] + frame[i+2][j+1] == 0)   
+    elif typeNo == 7 :
+        return (frame[i][j] + frame[i][j+1] + \
+                frame[i+1][j+1] + frame[i+2][j+1] == 0)
+    elif typeNo == 8 :
+        return (frame[i][j] + frame[i+1][j] + \
+                frame[i+1][j-1] + frame[i+1][j-2] == 0)
+    elif typeNo == 9 :
+        return (frame[i][j] + frame[i][j+1] + \
+                frame[i][j+2] + frame[i+1][j+1] == 0)
+    elif typeNo == 10 :
+        return (frame[i][j] + frame[i+1][j] + \
+                frame[i+2][j] + frame[i+1][j+1] == 0)
+    elif typeNo == 11 :
+        return (frame[i][j] + frame[i+1][j] + \
+                frame[i+2][j] + frame[i+1][j-1] == 0)
+    elif typeNo == 12 :
+        return (frame[i][j] + frame[i+1][j] + \
+                frame[i+1][j+1] + frame[i+1][j-1] == 0)
+    elif typeNo == 13 :
+        return (frame[i][j] + frame[i][j+1] + \
+                frame[i][j+2] + frame[i][j+3] == 0)
+    elif typeNo == 14 :
+        return (frame[i][j] + frame[i+1][j] + \
+                frame[i+2][j] + frame[i+3][j] == 0)    
+    elif typeNo == 15 :
+        return (frame[i][j] + frame[i][j+1] + \
+                frame[i+1][j+1] + frame[i+1][j] == 0)
 
 
-def do_Z1 :
-    global current
-    current.Z1 = copy.deepcopy(current)
-    current.data.append([i,j,'Z1'])
-    current.cnt_Z1 += 1
-    current.frame = copy.deepcopy(frame)
-    current.frame[i][j] = 1
-    current.frame[i+1][j] = 1
-    current.frame[i+1][j+1] = 1
-    current.frame[i+2][i+1] = 1
-def do_Z2 :
-    global current
-    current.Z2 = copy.deepcopy(current)
-    current.data.append([i,j,'Z2'])
-    current.cnt_Z2 += 1
-    current.frame = copy.deepcopy(frame)
-    current.frame[i][j] = 1
-    current.frame[i+1][j] = 1
-    current.frame[i+1][j+1] = 1
-    current.frame[i+2][i+1] = 1
-def do_J1 :
-    global current
-    current.J1 = copy.deepcopy(current)
-    current.data.append([i,j,'J1'])
-    current.cnt_J1 += 1
-    current.frame = copy.deepcopy(frame)
-    current.frame[i][j] = 1
-    current.frame[i+1][j] = 1
-    current.frame[i+1][j+1] = 1
-    current.frame[i+2][i+1] = 1
-def do_J2 :
-    global current
-    current.J2 = copy.deepcopy(current)
-    current.data.append([i,j,'J2'])
-    current.cnt_J2 += 1
-    current.frame = copy.deepcopy(frame)
-    current.frame[i][j] = 1
-    current.frame[i+1][j] = 1
-    current.frame[i+1][j+1] = 1
-    current.frame[i+2][i+1] = 1
-def do_J3 :
-    global current
-    current.J3 = copy.deepcopy(current)
-    current.data.append([i,j,'J3'])
-    current.cnt_J3 += 1
-    current.frame = copy.deepcopy(frame)
-    current.frame[i][j] = 1
-    current.frame[i+1][j] = 1
-    current.frame[i+1][j+1] = 1
-    current.frame[i+2][i+1] = 1
-def do_L1 :
-    global current
-    current.L1 = copy.deepcopy(current)
-    current.data.append([i,j,'L1'])
-    current.cnt_S += 1
-    current.frame = copy.deepcopy(frame)
-    current.frame[i][j] = 1
-    current.frame[i+1][j] = 1
-    current.frame[i+1][j+1] = 1
-    current.frame[i+2][i+1] = 1
-def do_L2 :
-    global current
-    current.L2 = copy.deepcopy(current)
-    current.data.append([i,j,'L2'])
-    current.cnt_L2 += 1
-    current.frame = copy.deepcopy(frame)
-    current.frame[i][j] = 1
-    current.frame[i+1][j] = 1
-    current.frame[i+1][j+1] = 1
-    current.frame[i+2][i+1] = 1
-def do_L3 :
-    global current
-    current.L3 = copy.deepcopy(current)
-    current.data.append([i,j,'L3'])
-    current.cnt_L3 += 1
-    current.frame = copy.deepcopy(frame)
-    current.frame[i][j] = 1
-    current.frame[i+1][j] = 1
-    current.frame[i+1][j+1] = 1
-    current.frame[i+2][i+1] = 1
-def do_T1 :
-    global current
-    current.T1 = copy.deepcopy(current)
-    current.data.append([i,j,'T1'])
-    current.cnt_T1 += 1
-    current.frame = copy.deepcopy(frame)
-    current.frame[i][j] = 1
-    current.frame[i+1][j] = 1
-    current.frame[i+1][j+1] = 1
-    current.frame[i+2][i+1] = 1
-def do_T2 :
-    global current
-    current.T2 = copy.deepcopy(current)
-    current.data.append([i,j,'T2'])
-    current.cnt_T2 += 1
-    current.frame = copy.deepcopy(frame)
-    current.frame[i][j] = 1
-    current.frame[i+1][j] = 1
-    current.frame[i+1][j+1] = 1
-    current.frame[i+2][i+1] = 1
-def do_T3 :
-    global current
-    current.T3 = copy.deepcopy(current)
-    current.data.append([i,j,'T3'])
-    current.cnt_T3 += 1
-    current.frame = copy.deepcopy(frame)
-    current.frame[i][j] = 1
-    current.frame[i+1][j] = 1
-    current.frame[i+1][j+1] = 1
-    current.frame[i+2][i+1] = 1
-def do_T4 :
-    global current
-    current.T4 = copy.deepcopy(current)
-    current.data.append([i,j,'T4'])
-    current.cnt_T4 += 1
-    current.frame = copy.deepcopy(frame)
-    current.frame[i][j] = 1
-    current.frame[i+1][j] = 1
-    current.frame[i+1][j+1] = 1
-    current.frame[i+2][i+1] = 1    
-def do_I1 :
-    global current
-    current.I1 = copy.deepcopy(current)
-    current.data.append([i,j,'I1'])
-    current.cnt_I1 += 1
-    current.frame = copy.deepcopy(frame)
-    current.frame[i][j] = 1
-    current.frame[i+1][j] = 1
-    current.frame[i+1][j+1] = 1
-    current.frame[i+2][i+1] = 1
-def do_I2 :
-    global current
-    current.I2 = copy.deepcopy(current)
-    current.data.append([i,j,'I2'])
-    current.cnt_I2 += 1
-    current.frame = copy.deepcopy(frame)
-    current.frame[i][j] = 1
-    current.frame[i+1][j] = 1
-    current.frame[i+1][j+1] = 1
-    current.frame[i+2][i+1] = 1
-def do_O :
-    global current
-    current.O = copy.deepcopy(current)
-    current.data.append([i,j,'O'])
-    current.cnt_O += 1
-    current.frame = copy.deepcopy(frame)
-    current.frame[i][j] = 1
-    current.frame[i+1][j] = 1
-    current.frame[i+1][j+1] = 1
-    current.frame[i+2][i+1] = 1
+def blockSwitch(typeNo, i, j, frame) : #add or remove a block
+    # temp_frame = copy.deepcopy(frame)
+    if typeNo == 0 :
+        frame[i][j] ^= 1
+        frame[i+1][j] ^= 1
+        frame[i+1][j+1] ^= 1
+        frame[i+2][j+1] ^= 1
+    elif typeNo == 1 :
+        frame[i][j] ^= 1
+        frame[i][j+1] ^= 1
+        frame[i+1][j+1] ^= 1
+        frame[i+1][j+2] ^= 1
+    elif typeNo == 2 :
+        frame[i][j] ^= 1
+        frame[i+1][j] ^= 1
+        frame[i+1][j-1] ^= 1
+        frame[i+2][j-1] ^= 1
+    elif typeNo == 3 :
+        frame[i][j] ^= 1
+        frame[i][j+1] ^= 1
+        frame[i][j+2] ^= 1
+        frame[i+1][j+2] ^= 1
+    elif typeNo == 4 :
+        frame[i][j] ^= 1
+        frame[i+1][j] ^= 1
+        frame[i+2][j] ^= 1
+        frame[i+2][j-1] ^= 1
+    elif typeNo == 5 :
+        frame[i][j] ^= 1
+        frame[i+1][j] ^= 1
+        frame[i+1][j+1] ^= 1
+        frame[i+1][j+2] ^= 1
+    elif typeNo == 6 :
+        frame[i][j] ^= 1
+        frame[i+1][j] ^= 1
+        frame[i+2][j] ^= 1
+        frame[i+2][j+1] ^= 1
+    elif typeNo == 7 :
+        frame[i][j] ^= 1
+        frame[i][j+1] ^= 1
+        frame[i+1][j+1] ^= 1
+        frame[i+2][j+1] ^= 1
+    elif typeNo == 8 :
+        frame[i][j] ^= 1
+        frame[i+1][j] ^= 1
+        frame[i+1][j-1] ^= 1
+        frame[i+1][j-2] ^= 1
+    elif typeNo == 9 :
+        frame[i][j] ^= 1
+        frame[i][j+1] ^= 1
+        frame[i][j+2] ^= 1
+        frame[i+1][j+1] ^= 1
+    elif typeNo == 10 :
+        frame[i][j] ^= 1
+        frame[i+1][j] ^= 1
+        frame[i+2][j] ^= 1
+        frame[i+1][j+1] ^= 1
+    elif typeNo == 11 :
+        frame[i][j] ^= 1
+        frame[i+1][j] ^= 1
+        frame[i+2][j] ^= 1
+        frame[i+1][j-1] ^= 1
+    elif typeNo == 12 :
+        frame[i][j] ^= 1
+        frame[i+1][j] ^= 1
+        frame[i+1][j+1] ^= 1
+        frame[i+1][j-1] ^= 1
+    elif typeNo == 13 :
+        frame[i][j] ^= 1
+        frame[i][j+1] ^= 1
+        frame[i][j+2] ^= 1
+        frame[i][j+3] ^= 1
+    elif typeNo == 14 :
+        frame[i][j] ^= 1
+        frame[i+1][j] ^= 1
+        frame[i+2][j] ^= 1
+        frame[i+3][j] ^= 1
+    elif typeNo == 15 :
+        frame[i][j] ^= 1
+        frame[i][j+1] ^= 1
+        frame[i+1][j+1] ^= 1
+        frame[i+1][j] ^= 1
+    else :
+        print("error : unknown typeNo")
 
+'''
+def nowFrame(history, frame): # history = [(typeNo, i, j), ... ]
+    for blockData in history:
+        blockSwitch(*blockData, frame)
+'''
 
 
 
@@ -251,14 +184,8 @@ def isComplete(frame) :
 
 
 
-
-def find_Cases(puzzle_frame):
-    global SIZE
-    for i, j in range(3, SIZE + 3):
-        pass
-
 # sample frame(for test)
-def loadFrame() :
+def load_frame() :
     frame = \
     [
         [ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 ],
@@ -276,15 +203,44 @@ def loadFrame() :
         [ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 ],
         [ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 ],
         [ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 ],
-    ]    
-
+    ]
+    return frame
 
 
 # frame of puzzle, size of FoP == SIZE + 3 + 3 (including borderlines)
 
 
+def currentFrame() :
+    global history, frame
+    current_frame = copy.deepcopy(frame)
+    for blockData in history : # history = [(typeNo, i, j), ...]
+        blockSwitch(*blockData, current_frame)
+    return current_frame
+
+def findCases() :
+    global history, frame    
+    for typeNo in range(16) :
+        current_frame = currentFrame()
+        i, j = findNext(current_frame)
+        if canAdd(typeNo, i, j, current_frame) :
+            blockSwitch(typeNo, i, j, current_frame)
+            history.append((typeNo, i, j))
+            cnt[typeNo] += 1
+            findCases()
+        else :
+            cnt[history.pop()[0]] -= 1
+            break
 
 
 ## Main
+def main():
+    global history, frame
+    frame = load_frame()
 
+    findCases()
     
+    
+
+
+if __name__ == "__main__" :
+    main()
