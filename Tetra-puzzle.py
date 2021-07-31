dcac5dd8c07a60638425234b654f0f601b99f2b0
pepze21 <pepze21@gmail.com>
2021-07-31 23:07:25 +0900

    add 3 missing cases

--- Tetra-puzzle.py
+++ Tetra-puzzle.py
@@ -1,166 +1,174 @@
-### Tetra Puzzle ver.210727.04 rough draft, shortened the length of the code by using indices
+### Tetra Puzzle ver.210731.05, add 3 missing cases
 
-
-import copy # to use deepcopy()
+import copy
 
 ## Variables
 SIZE = 9
 frame = []
 history = []
-cnt = [0 for _ in range(16)]
+solutions = []
 
-## Class & Functions
-
-'''
-class Node() :
-    def __init__(self):
-        self.frame = None # frame state
-        self.blockData = [] # blocks filled in initial frame
-        self.type = [None for _ in range(16)] #1S2Z3J3L4T2I1O:16cases
-        self.cnt = [0 for _ in range(16)]
-'''
-
+## Functions
 def findNext(frame):
     for i in range(3, SIZE + 3):
         for j in range(3, SIZE + 3):
             if (frame[i][j] == 0):
-                return i, j # return (i, j) of tuple type
+                return i, j
 
-def isEmpty(i, j) :
-    return (frame[i][j] == 0)
-
-
-# typeNo : 0:S  / 1:Z1 /  2:Z2 /  3:J1 /  4:J2 /  5:J3 /  6:L1 /  7:L2
-#          8:L3 / 9:T1 / 10:T2 / 11:T3 / 12:T4 / 13:I1 / 14:I2 / 15:O
+# typeNo : 0:S1 /  1:S2 /  2:Z1 /  3:Z2 /  4:J1 /  5:J2 /  6:J3 /
+#          7:J4 /  8:L1 /  9:L2 / 10:L3 / 11:L4 / 12:T1 / 13:T2 /
+#         14:T3 / 15:T4 / 16:I1 / 17:I2 / 18:O
 def canAdd(typeNo, i, j, frame) :
     if typeNo == 0 :
         return (frame[i][j] + frame[i+1][j] + \
-                frame[i+1][j+1] + frame[i+2][j+1] == 0)                
-    elif typeNo == 1 :
+                frame[i+1][j+1] + frame[i+2][j+1] == 0)
+    elif typeNo == 1 : # new1
+        return (frame[i][j] + frame[i][j+1] + \
+                frame[i+1][j] + frame[i+1][j-1] == 0)
+    elif typeNo == 2 :
         return (frame[i][j] + frame[i][j+1] + \
                 frame[i+1][j+1] + frame[i+1][j+2] == 0)
-    elif typeNo == 2 :
+    elif typeNo == 3 :
         return (frame[i][j] + frame[i+1][j] + \
                 frame[i+1][j-1] + frame[i+2][j-1] == 0)    
-    elif typeNo == 3 :
+    elif typeNo == 4 :
         return (frame[i][j] + frame[i][j+1] + \
                 frame[i][j+2] + frame[i+1][j+2] == 0)
-    elif typeNo == 4 :
-        return (frame[i][j] + frame[i+1][j] + \
-                frame[i+2][j] + frame[i+2][j-1] == 0)    
     elif typeNo == 5 :
         return (frame[i][j] + frame[i+1][j] + \
-                frame[i+1][j+1] + frame[i+1][j+2] == 0)    
+                frame[i+2][j] + frame[i+2][j-1] == 0)    
     elif typeNo == 6 :
         return (frame[i][j] + frame[i+1][j] + \
-                frame[i+2][j] + frame[i+2][j+1] == 0)   
-    elif typeNo == 7 :
+                frame[i+1][j+1] + frame[i+1][j+2] == 0)
+    elif typeNo == 7 : # new 7
         return (frame[i][j] + frame[i][j+1] + \
-                frame[i+1][j+1] + frame[i+2][j+1] == 0)
+                frame[i+1][j] + frame[i+2][j] == 0)    
     elif typeNo == 8 :
         return (frame[i][j] + frame[i+1][j] + \
-                frame[i+1][j-1] + frame[i+1][j-2] == 0)
+                frame[i+2][j] + frame[i+2][j+1] == 0)   
     elif typeNo == 9 :
         return (frame[i][j] + frame[i][j+1] + \
-                frame[i][j+2] + frame[i+1][j+1] == 0)
+                frame[i+1][j+1] + frame[i+2][j+1] == 0)
     elif typeNo == 10 :
         return (frame[i][j] + frame[i+1][j] + \
-                frame[i+2][j] + frame[i+1][j+1] == 0)
-    elif typeNo == 11 :
-        return (frame[i][j] + frame[i+1][j] + \
-                frame[i+2][j] + frame[i+1][j-1] == 0)
-    elif typeNo == 12 :
-        return (frame[i][j] + frame[i+1][j] + \
-                frame[i+1][j+1] + frame[i+1][j-1] == 0)
-    elif typeNo == 13 :
+                frame[i+1][j-1] + frame[i+1][j-2] == 0)
+    elif typeNo == 11 : # new 11
         return (frame[i][j] + frame[i][j+1] + \
-                frame[i][j+2] + frame[i][j+3] == 0)
+                frame[i][j+2] + frame[i+1][j] == 0)
+    elif typeNo == 12 :
+        return (frame[i][j] + frame[i][j+1] + \
+                frame[i][j+2] + frame[i+1][j+1] == 0)
+    elif typeNo == 13 :
+        return (frame[i][j] + frame[i+1][j] + \
+                frame[i+2][j] + frame[i+1][j+1] == 0)
     elif typeNo == 14 :
         return (frame[i][j] + frame[i+1][j] + \
-                frame[i+2][j] + frame[i+3][j] == 0)    
+                frame[i+2][j] + frame[i+1][j-1] == 0)
     elif typeNo == 15 :
+        return (frame[i][j] + frame[i+1][j] + \
+                frame[i+1][j+1] + frame[i+1][j-1] == 0)
+    elif typeNo == 16 :
+        return (frame[i][j] + frame[i][j+1] + \
+                frame[i][j+2] + frame[i][j+3] == 0)
+    elif typeNo == 17 :
+        return (frame[i][j] + frame[i+1][j] + \
+                frame[i+2][j] + frame[i+3][j] == 0)    
+    elif typeNo == 18 :
         return (frame[i][j] + frame[i][j+1] + \
                 frame[i+1][j+1] + frame[i+1][j] == 0)
 
-
-def blockSwitch(typeNo, i, j, frame) : #add or remove a block
-    # temp_frame = copy.deepcopy(frame)
+def blockSwitch(typeNo, i, j, frame) :
     if typeNo == 0 :
         frame[i][j] ^= 1
         frame[i+1][j] ^= 1
         frame[i+1][j+1] ^= 1
         frame[i+2][j+1] ^= 1
-    elif typeNo == 1 :
+    elif typeNo == 1 : # new1
+        frame[i][j] ^= 1
+        frame[i][j+1] ^= 1
+        frame[i+1][j] ^= 1
+        frame[i+1][j-1] ^= 1
+    elif typeNo == 2 :
         frame[i][j] ^= 1
         frame[i][j+1] ^= 1
         frame[i+1][j+1] ^= 1
         frame[i+1][j+2] ^= 1
-    elif typeNo == 2 :
+    elif typeNo == 3 :
         frame[i][j] ^= 1
         frame[i+1][j] ^= 1
         frame[i+1][j-1] ^= 1
         frame[i+2][j-1] ^= 1
-    elif typeNo == 3 :
+    elif typeNo == 4 :
         frame[i][j] ^= 1
         frame[i][j+1] ^= 1
         frame[i][j+2] ^= 1
         frame[i+1][j+2] ^= 1
-    elif typeNo == 4 :
+    elif typeNo == 5 :
         frame[i][j] ^= 1
         frame[i+1][j] ^= 1
         frame[i+2][j] ^= 1
         frame[i+2][j-1] ^= 1
-    elif typeNo == 5 :
+    elif typeNo == 6 :
         frame[i][j] ^= 1
         frame[i+1][j] ^= 1
         frame[i+1][j+1] ^= 1
         frame[i+1][j+2] ^= 1
-    elif typeNo == 6 :
+    elif typeNo == 7 : # new7
+        frame[i][j] ^= 1
+        frame[i][j+1] ^= 1
+        frame[i+1][j] ^= 1
+        frame[i+2][j] ^= 1
+    elif typeNo == 8 :
         frame[i][j] ^= 1
         frame[i+1][j] ^= 1
         frame[i+2][j] ^= 1
         frame[i+2][j+1] ^= 1
-    elif typeNo == 7 :
+    elif typeNo == 9 :
         frame[i][j] ^= 1
         frame[i][j+1] ^= 1
         frame[i+1][j+1] ^= 1
         frame[i+2][j+1] ^= 1
-    elif typeNo == 8 :
+    elif typeNo == 10 :
         frame[i][j] ^= 1
         frame[i+1][j] ^= 1
         frame[i+1][j-1] ^= 1
         frame[i+1][j-2] ^= 1
-    elif typeNo == 9 :
+    elif typeNo == 11 : # new11
         frame[i][j] ^= 1
         frame[i][j+1] ^= 1
         frame[i][j+2] ^= 1
-        frame[i+1][j+1] ^= 1
-    elif typeNo == 10 :
-        frame[i][j] ^= 1
         frame[i+1][j] ^= 1
-        frame[i+2][j] ^= 1
-        frame[i+1][j+1] ^= 1
-    elif typeNo == 11 :
-        frame[i][j] ^= 1
-        frame[i+1][j] ^= 1
-        frame[i+2][j] ^= 1
-        frame[i+1][j-1] ^= 1
     elif typeNo == 12 :
         frame[i][j] ^= 1
-        frame[i+1][j] ^= 1
-        frame[i+1][j+1] ^= 1
-        frame[i+1][j-1] ^= 1
-    elif typeNo == 13 :
-        frame[i][j] ^= 1
         frame[i][j+1] ^= 1
         frame[i][j+2] ^= 1
-        frame[i][j+3] ^= 1
+        frame[i+1][j+1] ^= 1
+    elif typeNo == 13 :
+        frame[i][j] ^= 1
+        frame[i+1][j] ^= 1
+        frame[i+2][j] ^= 1
+        frame[i+1][j+1] ^= 1
     elif typeNo == 14 :
         frame[i][j] ^= 1
         frame[i+1][j] ^= 1
         frame[i+2][j] ^= 1
-        frame[i+3][j] ^= 1
+        frame[i+1][j-1] ^= 1
     elif typeNo == 15 :
+        frame[i][j] ^= 1
+        frame[i+1][j] ^= 1
+        frame[i+1][j+1] ^= 1
+        frame[i+1][j-1] ^= 1
+    elif typeNo == 16 :
+        frame[i][j] ^= 1
+        frame[i][j+1] ^= 1
+        frame[i][j+2] ^= 1
+        frame[i][j+3] ^= 1
+    elif typeNo == 17 :
+        frame[i][j] ^= 1
+        frame[i+1][j] ^= 1
+        frame[i+2][j] ^= 1
+        frame[i+3][j] ^= 1
+    elif typeNo == 18 :
         frame[i][j] ^= 1
         frame[i][j+1] ^= 1
         frame[i+1][j+1] ^= 1
@@ -168,24 +176,29 @@ def blockSwitch(typeNo, i, j, frame) : #add or remove a block
     else :
         print("error : unknown typeNo")
 
-'''
-def nowFrame(history, frame): # history = [(typeNo, i, j), ... ]
-    for blockData in history:
-        blockSwitch(*blockData, frame)
-'''
-
-
-
 def isComplete(frame) :
-    for i, j in range(SIZE + 3):
-        if (frame[i][j] == 0) :
-            return False
+    for i in range(3, SIZE + 3):
+        for j in range(3, SIZE +3):
+            if (frame[i][j] == 0) :
+                return False
     return True
 
-
-
+def cnt_ClassifiedBlockType(history) :
+    cnt = [0 for _ in range(16)]
+    for blockData in history :
+        cnt[blockData[0]] += 1
+    cnt_classified = [0 for _ in range(7)]
+    cnt_classified[0] = cnt[0]
+    cnt_classified[1] = cnt[1]  + cnt[2]
+    cnt_classified[2] = cnt[3]  + cnt[4]  + cnt[5]
+    cnt_classified[3] = cnt[6]  + cnt[7]  + cnt[8]
+    cnt_classified[4] = cnt[9]  + cnt[10] + cnt[11] + cnt[12]
+    cnt_classified[5] = cnt[13] + cnt[14]
+    cnt_classified[6] = cnt[15]
+    return cnt_classified
+    
 # sample frame(for test)
-def load_frame() :
+def loadFrame() :
     frame = \
     [
         [ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 ],
@@ -206,41 +219,33 @@ def load_frame() :
     ]
     return frame
 
-
-# frame of puzzle, size of FoP == SIZE + 3 + 3 (including borderlines)
-
-
-def currentFrame() :
-    global history, frame
-    current_frame = copy.deepcopy(frame)
-    for blockData in history : # history = [(typeNo, i, j), ...]
-        blockSwitch(*blockData, current_frame)
-    return current_frame
-
 def findCases() :
-    global history, frame    
+    global history, frame, solutions
     for typeNo in range(16) :
-        current_frame = currentFrame()
-        i, j = findNext(current_frame)
-        if canAdd(typeNo, i, j, current_frame) :
-            blockSwitch(typeNo, i, j, current_frame)
-            history.append((typeNo, i, j))
-            cnt[typeNo] += 1
-            findCases()
-        else :
-            cnt[history.pop()[0]] -= 1
+        if isComplete(frame) :
+            solutions.append(copy.deepcopy(history))
+            blockSwitch(*history.pop(), frame)
             break
+        i, j = findNext(frame)
+        if canAdd(typeNo, i, j, frame) :
+            blockSwitch(typeNo, i, j, frame)
+            history.append((typeNo, i, j))
+            findCases()
+        elif typeNo == 15 :
+            blockSwitch(*history.pop(), frame)
 
-
-## Main
 def main():
-    global history, frame
-    frame = load_frame()
+    global history, frame, solutions
+    initial_frame = loadFrame()
+    frame = copy.deepcopy(initial_frame)
 
     findCases()
     
-    
+    for history in solutions :
+        print(history)
+        print("S Z J L T I O 갯수")
+        print(cnt_ClassifiedBlockType(history))
 
-
+## Main
 if __name__ == "__main__" :
     main()
